e6b65b01dbef3be657bed404c1e616f0552d03e3
HansDreyfuss <egozador@yandex.ru>
2022-04-24 16:33:49 +0300

    commit

--- app.py
+++ app.py
@@ -6,15 +6,28 @@ path_pdf = 'Lib/Curricula.pdf'
 
 
 def graph():
-    data = [
-        ["смотреть", "подолгу"], ["смотреть", "детьми"],
-        ["подолгу", "спать"], ["подолгу", "сидеть"],
-        ["спать", "сладко"], ["спать", "крепко"],
-        ["детьми", "воспитывать"], ["детьми", "ухаживать"]
+
+    categories = ["Человеческие, организационные и нормативные аспекты"]
+
+    domains = [
+        "Управление рисками и непрерывностью бизнеса", "Юридические и нормативные аспекты ИБ",
+        "Человеческие факторы в ИБ", "ИБ онлайн-деятельности"
     ]
+
+    data = [
+        [categories[0], domains[0]],
+        [categories[0], domains[1]],
+        [categories[0], domains[2]],
+        [categories[0], domains[3]],
+    ]
+
+    for i in range(len(categories)):
+        for j in range(len(domains)):
+            data[(i+1)*j] = [categories[i], domains[j]]
+
     weights = {
-        'смотреть': 2, 'подолгу': 1.5, 'детьми': 1.5, 'спать': 1, 'сидеть': 1, 'сладко': 1,
-        'крепко': 1, 'воспитывать': 1, 'ухаживать': 1
+        categories[0]: 16,
+        domains[0]: 8, domains[1]: 8, domains[2]: 8, domains[3]: 8
     }
     attrs = {key: {"weight": val} for key, val in weights.items()}
     g = nx.Graph()
@@ -28,12 +41,22 @@ def graph():
 if __name__ == "__main__":
     content = ''
     with pp.open(path_pdf) as pdf:
-        for i in range(len(pdf.pages)):
+        for i in range(5, 7):  # (len(pdf.pages)):
             page = pdf.pages[i]
             page_content = '\n'.join(page.extract_text().split('\n')[:-1])
             content = content + page_content
     pdf.close()
     print(content)
+
+    # Категории свода знаний специальности кибербезопасность categories = []
+
+    # Подкатегории-домены для каждой категории domains = []
+
+    # Модули доменов modules = []
+
+    # Темы-результаты модулей results = []
+
+    # Навыки-цели skills = []
 
     graph()
     plt.show()
